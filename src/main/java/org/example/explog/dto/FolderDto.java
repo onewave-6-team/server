d0a58ddd7038278d0131784ae44114dde601ec04
KangAyeon05 <rkddkdus05@gmail.com>
2026-02-07 01:39:01 +0900

    lombokVer

--- src/main/java/org/example/explog/dto/FolderDto.java
+++ src/main/java/org/example/explog/dto/FolderDto.java
@@ -1,34 +1,16 @@
 package org.example.explog.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import java.time.LocalDateTime;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class FolderDto {
-
     private Long folderId;
     private String folderType;
     private String name;
     private LocalDateTime updatedAt;
-
-    public FolderDto(Long folderId, String folderType, String name, LocalDateTime updatedAt) {
-        this.folderId = folderId;
-        this.folderType = folderType;
-        this.name = name;
-        this.updatedAt = updatedAt;
-    }
-
-    public Long getFolderId() {
-        return folderId;
-    }
-
-    public String getFolderType() {
-        return folderType;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public LocalDateTime getUpdatedAt() {
-        return updatedAt;
-    }
 }
--- src/main/java/org/example/explog/dto/FolderResponseDto.java
+++ src/main/java/org/example/explog/dto/FolderResponseDto.java
@@ -1,16 +1,13 @@
 package org.example.explog.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import java.util.List;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class FolderResponseDto {
-
     private List<FolderDto> folders;
-
-    public FolderResponseDto(List<FolderDto> folders) {
-        this.folders = folders;
-    }
-
-    public List<FolderDto> getFolders() {
-        return folders;
-    }
 }
